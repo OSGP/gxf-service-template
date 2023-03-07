36a4cba1bd8f4af57f650cab97cd3c2ad06d1c77
Jasper Kamerling <jasper.kamerling@alliander.com>
2023-03-07 09:58:23 +0100

    Move client factory to configuration class
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

---
+++ src/main/kotlin/com/gxf/servicetemplate/mqtt/MqttConfiguration.kt
@@ -0,0 +1,24 @@
+package com.gxf.servicetemplate.mqtt
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory
+
+@Configuration
+class MqttConfiguration {
+
+    @Bean
+    fun mqttClientFactory(): MqttPahoClientFactory {
+        val factory = DefaultMqttPahoClientFactory()
+        val options = MqttConnectOptions()
+        options.serverURIs = arrayOf("tcp://localhost:1883")
+        options.userName = "admin"
+        options.password = "password".toCharArray()
+        options.isAutomaticReconnect = true
+        factory.connectionOptions = options
+        return factory
+    }
+
+}
