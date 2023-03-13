5bc1a021ac7b3d4490d0d5552a92b4dd65ac632f
Jasper Kamerling <jasper.kamerling@alliander.com>
2023-03-13 10:02:49 +0100

    Publish docker image to GitHub packages
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,3 +77,19 @@ testing {
         }
     }
 }
+
+springBoot {
+    buildInfo()
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+    imageName.set(System.getenv("REGISTRY"))
+    publish.set(true)
+    docker {
+        publishRegistry {
+            username.set(System.getenv("GITHUB_ACTOR"))
+            password.set(System.getenv("GITHUB_TOKEN"))
+        }
+    }
+}
+
