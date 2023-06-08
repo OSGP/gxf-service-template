00129ba2217381b14a7be650902c7f635e194106
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-06-08 16:00:47 +0200

    added licesing information
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- build-logic/src/main/kotlin/gxf.avro.gradle.kts
+++ build-logic/src/main/kotlin/gxf.avro.gradle.kts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 plugins {
     id("gxf.spring-conventions")
     id("com.github.davidmc24.gradle.plugin.avro")
--- build-logic/src/main/kotlin/gxf.spring-conventions.gradle.kts
+++ build-logic/src/main/kotlin/gxf.spring-conventions.gradle.kts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
