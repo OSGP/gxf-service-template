26e47dc19f1bf5d64e0bd8f477b2df1d17465112
Jasper Kamerling <jasper.kamerling@alliander.com>
2023-09-28 14:22:59 +0200

    Update integration test
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- application/src/integrationTest/kotlin/org/gxf/servicetemplate/GxfServiceTemplateApplicationTests.kt
+++ application/src/integrationTest/kotlin/org/gxf/servicetemplate/GxfServiceTemplateApplicationTests.kt
@@ -6,8 +6,10 @@ package org.gxf.servicetemplate
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
 
 @SpringBootTest
+@EmbeddedKafka(topics = ["avroTopic"])
 class GxfServiceTemplateApplicationTests {
 
     @Test
