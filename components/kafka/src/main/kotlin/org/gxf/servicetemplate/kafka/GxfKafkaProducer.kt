42f2dd8ee065c3c2ebaa5c8e7f237b85436fc463
Jasper Kamerling <15668493+jasperkamerling@users.noreply.github.com>
2024-01-09 14:42:19 +0100

    FDP-1736: Upgrade Gradle, Java and Spring Boot (#13)
    * FDP-1736: Upgrade Gradle, Java and Spring Boot
    
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>
    
    * FDP-1736: Take loggers out of companion objects
    
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>
    
    ---------
    
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- components/kafka/src/main/kotlin/org/gxf/servicetemplate/kafka/GxfKafkaProducer.kt
+++ components/kafka/src/main/kotlin/org/gxf/servicetemplate/kafka/GxfKafkaProducer.kt
@@ -4,9 +4,8 @@
 
 package org.gxf.servicetemplate.kafka
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.gxf.service.Measurement
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -19,14 +18,12 @@ class GxfKafkaProducer(
     private val measurementGenerator: MeasurementGenerator
 ) {
 
-    companion object {
-        private val logger: Logger = LoggerFactory.getLogger(this::class.java)
-    }
+    private val logger = KotlinLogging.logger { }
 
     @Scheduled(cron = "* * * * * *")
     fun producer() {
         val deviceId = Random.nextLong()
-        logger.info("Producing: ${deviceId}")
+        logger.info { "Producing: ${deviceId}" }
         kafkaTemplate.send("avroTopic", measurementGenerator.generateMeasurement(deviceId))
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,26 @@ include("application")
 include("components:avro")
 include("components:kafka")
 include("components:mqtt")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("logging", "io.github.oshai", "kotlin-logging-jvm").version("6.0.1")
+
+            version("mapstruct", "1.5.5.Final")
+            library("mapstruct", "org.mapstruct", "mapstruct").versionRef("mapstruct")
+            library("mapstructannotation", "org.mapstruct", "mapstruct-processor").versionRef("mapstruct")
+
+            library("avro", "org.apache.avro", "avro").version("1.11.3")
+
+            version("gxfUtils", "0.2")
+            library("kafkaAvro", "com.gxf.utilities", "kafka-avro").versionRef("gxfUtils")
+            library("kafkaAzureOAuth", "com.gxf.utilities", "kafka-azure-oauth").versionRef("gxfUtils")
+        }
+        create("integrationTestLibs") {
+            version("testContainers", "1.19.3")
+            library("kafkaTestContainers", "org.testcontainers", "kafka").versionRef("testContainers")
+
+        }
+    }
+}
