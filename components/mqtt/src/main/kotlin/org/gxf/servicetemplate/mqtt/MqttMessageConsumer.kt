1591cde68686d993c049f922e145ae09a2a1605e
Loes Immens <loes.immens@alliander.com>
2024-10-03 09:59:06 +0200

    FDP-2318: fixed copyright text used
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- components/mqtt/src/main/kotlin/org/gxf/servicetemplate/mqtt/MqttMessageConsumer.kt
+++ components/mqtt/src/main/kotlin/org/gxf/servicetemplate/mqtt/MqttMessageConsumer.kt
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Contributors to the GXF project
+// SPDX-FileCopyrightText: Copyright Contributors to the GXF project
 //
 // SPDX-License-Identifier: Apache-2.0
 package org.gxf.servicetemplate.mqtt
