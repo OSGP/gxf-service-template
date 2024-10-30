eb2ddf7f7e2aaa3e5947a19a4a8ca464c2bd5eee
Loes Immens <loes.immens@alliander.com>
2024-10-30 16:49:01 +0100

    FDP-2728: update spring boot and kotlin plugins
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,11 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.3.4" apply false
+    id("org.springframework.boot") version "3.3.5" apply false
     id("io.spring.dependency-management") version "1.1.6" apply false
-    kotlin("jvm") version "2.0.20" apply false
-    kotlin("plugin.spring") version "2.0.20" apply false
-    kotlin("plugin.jpa") version "2.0.20" apply false
+    kotlin("jvm") version "2.0.21" apply false
+    kotlin("plugin.spring") version "2.0.21" apply false
+    kotlin("plugin.jpa") version "2.0.21" apply false
     id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1" apply false
     id("org.sonarqube") version "5.1.0.4882"
     id("com.diffplug.spotless") version("6.25.0")
